4dddff3d3e30ffe97ca6b723a126418e654d3d5e
Goshiz <goshiz@goshiz.io>
2021-03-19 17:13:58 +0100

    change subsitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ aws_access_key_id = ${SPACE_ACCESS_KEY_ID}
 aws_secret_access_key = ${SPACE_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 
-aws s3 cp ${SOURCE_DIR} s3://${SPACE_NAME}/${SPACE_FOLDER}/${GITHUB_REF#refs/heads/}/ \
+aws s3 cp ${SOURCE_DIR} s3://${SPACE_NAME}/${SPACE_FOLDER}/${GITHUB_REF##*/}/ \
             --endpoint https://${SPACE_REGION}.digitaloceanspaces.com $*
 
 rm -rf ~/.aws
